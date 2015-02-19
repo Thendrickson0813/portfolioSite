e12d3f21abb724bc208a6df1639a989748ab173f
Timothy Hendrickson <Thendrickson@gmail.com>
2015-02-19 10:15:43 -0500

    commit portfolio

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+   
+        
+
+    // owl.carousel-------------------------------
+
+     $("#owl-demo").owlCarousel({
+ 
+navigation : false, // Show next and prev buttons
+slideSpeed : 300,
+paginationSpeed : 400,
+singleItem:true,
+autoPlay: true, 
+// "singleItem:true" is a shortcut for:
+ items : 1,
+// itemsDesktop : false,
+// itemsDesktopSmall : false,
+// itemsTablet: false,
+// itemsMobile : false
+ 
+});
+
+    // owl.carousel second one on page.
+    // owl.carousel-------------------------------
+
+       $("#owl-demo2").owlCarousel({
+ 
+autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+items : 4,
+itemsDesktop : [1199,3],
+itemsDesktopSmall : [979,3]
+ 
+});
+ // dropdown mouse over   
+    $('.dropdown-toggle').dropdownHover(options);
+      
+});
