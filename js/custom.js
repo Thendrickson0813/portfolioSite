9eb2d2e22c135530e723922ad6c4aba9169ea09e
Timothy Hendrickson <Thendrickson@gmail.com>
2015-07-17 16:05:31 -0400

    Issues fixed site is live at http://psd.reen.timothy-hendrickson.com/ still needs some fixes but minor

--- js/custom.js
+++ js/custom.js
@@ -1,39 +1,41 @@
 $(document).ready(function() {
 
-   
-        
+
+
 
     // owl.carousel-------------------------------
 
-     $("#owl-demo").owlCarousel({
- 
-navigation : false, // Show next and prev buttons
-slideSpeed : 600,
-paginationSpeed : 400,
-singleItem:true,
-autoPlay: true, 
-// "singleItem:true" is a shortcut for:
- items : 1,
-// itemsDesktop : false,
-// itemsDesktopSmall : false,
-// itemsTablet: false,
-// itemsMobile : false
- 
-});
+    $("#owl-demo").owlCarousel({
+
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 600,
+        paginationSpeed: 400,
+        singleItem: true,
+        autoPlay: true
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+
+    });
 
     // owl.carousel second one on page.
     // owl.carousel-------------------------------
 
-       $("#owl-demo2").owlCarousel({
- 
-autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
-items : 4,
-itemsDesktop : [1199,3],
-itemsDesktopSmall : [979,3]
- 
-});
- // dropdown mouse over   
+    $("#owl-demo2").owlCarousel({
+
+        // autoPlay: 3000, //Set AutoPlay to 3 seconds
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 600,
+        paginationSpeed: 400,
+        autoPlay: true,
+        items: 4
+        // itemsDesktop: [1199, 3],
+        // itemsDesktopSmall: [979, 8]
+
+    });
+    // dropdown mouse over   
     $('.dropdown-toggle').dropdownHover(options);
-      
+    
+
 });
